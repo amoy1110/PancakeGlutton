9666740d696c7c1d8e35e42f1965f51d598aff3f
Adam Moy <a.moy1110@gmail.com>
2017-02-07 21:28:20 -0500

    Added GetLeastEatenPancakes() and GetPersonWithMostEaten() to Utility.cpp

--- PancakeGlutton/PancakeGlutton/Utility.h
+++ PancakeGlutton/PancakeGlutton/Utility.h
@@ -1,5 +1,7 @@
+#pragma once
 #include "Person.h"
 
 int GetMostEatenPancakes(Person personList[], int size);
-
+int GetLeastEatenPancakes(Person personList[], int size);
+int GetPersonWithMostEaten(Person personList[], int size);
 
